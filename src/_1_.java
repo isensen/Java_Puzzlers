50ac10cfbdb6e86192cb61b64950324caa20f11a
isensen <isensen@yahoo.cn>
2013-06-17 21:45:43 +0800

    init

---
+++ src/_1_.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: i33
+ * Date: 13-6-17
+ * Time: 下午9:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class _1_ {
+
+    public static void main(String... args){
+        System.out.println();
+    }
+}
