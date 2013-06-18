9c60e811d01f67e4c051d70596905ae109bd3781
isensen <isensen@yahoo.cn>
2013-06-18 11:30:54 +0800

    1

--- src/_1_isOdd.java
+++ src/_1_isOdd.java
@@ -7,7 +7,34 @@
  */
 public class _1_isOdd {
 
+    public static boolean isOdd0 (int i ){
+        return  i % 2 == 1;
+    }
+
+    public static boolean isOdd1 (int i){
+        return  i % 2 !=0;
+    }
+    public static boolean isOdd2 (int i){
+        return (i & 1) !=0;
+    }
     public static void main(String... args) {
-        System.out.println();
+        System.out.println(isOdd0(2));
+        System.out.println(isOdd0(1));
+        System.out.println(isOdd0(0));
+        System.out.println(isOdd0(-1));
+        System.out.println(isOdd0(-2));
+
+        System.out.println(isOdd1(2));
+        System.out.println(isOdd1(1));
+        System.out.println(isOdd1(0));
+        System.out.println(isOdd1(-1));
+        System.out.println(isOdd1(-2));
+
+        System.out.println(isOdd2(2));
+        System.out.println(isOdd2(1));
+        System.out.println(isOdd2(0));
+        System.out.println(isOdd2(-1));
+        System.out.println(isOdd2(-2));
+
     }
 }
