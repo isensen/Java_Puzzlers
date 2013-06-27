bee63b65e3024c6d88cc398de6435a5bc75eadd2
isensen <isensen@yahoo.cn>
2013-06-27 17:26:43 +0800

    11,12,13

---
+++ src/Two/_11_LastLaugh.java
@@ -0,0 +1,53 @@
+package Two;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: i33
+ * Date: 13-6-27
+ * Time: 下午4:02
+ */
+public class _11_LastLaugh {
+
+    public static void main(String[] args){
+        System.out.println("H"+"a");//双引号
+
+        // 'H'和'a'是字符型字面常量,因为这两个操作数都不是字符串类型的，所以 + 操作符执行的是加法而不是字符串连接。
+        System.out.println('H'+'a');//单引号
+
+        System.out.println("" + 'H' + 'a');
+
+        System.out.println("2 + 2 = " + 2+2);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append('H');
+        sb.append('a');
+        System.out.println(sb);
+
+        /*
+        最终结果: Ha169
+        编译器在计算常量表达式'H'+'a'时，是通过我们熟知的拓宽原始类型转换将两个具有字符型数值的操作数（'H'和'a'）
+        提升为int数值而实现的。从char到int的拓宽原始类型转换是将16位的char数值零扩展到32位的int。对于'H'，char
+        数值是72，而对于'a'，char数值是97，因此表达式'H'+'a'等价于int常量72 + 97，或169。
+
+        站在语言的立场上，若干个char和字符串的相似之处是虚幻的。语言所关心的是，char是一个无符号16位原始类型整数
+        ——仅此而已。对类库来说就不尽如此了，类库包含了许多可以接受char参数，并将其作为Unicode字符处理的方法。
+
+        那么你应该怎样将字符连接在一起呢？你可以使用这些类库。例如，你可以使用一个字符串缓冲区：
+
+            StringBuffer sb = new StringBuffer();
+            sb.append('H');
+            sb.append('a');
+            System.out.println(sb);
+
+        还可以通过确保至少有一个操作数为字符串类型，来强制 + 操作符去执行一个字符串连接操作，而不是一个加法操作。这种常见的惯用法用一个空字符串（""）作为一个连接序列的开始，如下所示：
+
+	        System.out.println("" + 'H' + 'a');
+
+        如果使用的是JDK 5.0，你还可以使用
+	        System.out.printf("%c%c", 'H', 'a');
+
+        */
+    }
+
+
+}
---
+++ src/Two/_12_ABC.java
@@ -0,0 +1,55 @@
+package Two;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: i33
+ * Date: 13-6-27
+ * Time: 下午4:11
+ */
+public class _12_ABC {
+
+    public static void main(String[] args){
+        String letters = "ABC";
+        char[] numbers = {'1', '2', '3'};
+        System.out.println(letters + " easy as " + numbers);
+
+
+        String letters2 = "ABC";
+        Object numbers2 = new char[] { '1', '2', '3' };
+        System.out.print(letters2 + " easy as ");
+        //它调用的是println的Object重载版本，而不是char[]重载版本。所以也不能达到输出的目的r
+        System.out.println(numbers2);
+
+    }
+        /*
+        尽管char是一个整数类型，但是许多类库都对其进行了特殊处理，因为char数值通常表示的是字符而不是整数。例如，将一个char数值
+        传递给println方法会打印出一个Unicode字符而不是它的数字代码。字符数组受到了相同的特殊处理：println的char[]重载版本会打
+        印出数组所包含的所有字符，而String.valueOf和StringBuffer.append的char[]重载版本的行为也是类似的。
+
+        然而，字符串连接操作符在这些方法中没有被定义。该操作符被定义为先对它的两个操作数执行字符串转换，然后将产生的两个字符串
+        连接到一起。对包括数组在内的对象引用的字符串转换定义如下：
+        如果引用为null，它将被转换成字符串"null"。否则，该转换的执行就像是不用任何参数调用该引用对象的toString方法一样；但是
+        如果调用toString方法的结果是null，那么就用字符串"null"来代替。
+
+        那么，在一个非空char数组上面调用toString方法会产生什么样的行为呢？数组是从Object那里继承的toString方法[JLS 10.7]，
+        规范中描述到：“返回一个字符串，它包含了该对象所属类的名字，'@'符号，以及表示对象散列码的一个无符号十六进制整数”[Java-API]。
+        有关Class.getName的规范描述到：在char[]类型的类对象上调用该方法的结果为字符串"[C"。将它们连接到一起就形成了在我们的程序
+        中打印出来的那个丑陋的字符串。
+
+        有两种方法可以订正这个程序。你可以在调用字符串连接操作之前，显式地将一个数组转换成一个字符串：
+
+            System.out.println(letters + " easy as " +
+                                   String.valueOf(numbers));
+
+        或者，你可以将System.out.println调用分解为两个调用，以利用println的char[]重载版本：
+
+            System.out.print(letters + " easy as ");
+            System.out.println(numbers);
+
+        总之，char数组不是字符串。要想将一个char数组转换成一个字符串，就要调用String.valueOf(char[])方法。某些类库中的方法提供
+        了对char数组的类似字符串的支持，通常是提供一个Object版本的重载方法和一个char[]版本的重载方法，而之后后者才能产生我们想要
+        的行为。
+        */
+
+
+}
