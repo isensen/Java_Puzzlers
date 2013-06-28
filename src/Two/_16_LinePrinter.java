95ae9c0add54f38b83f8a0cd8ec892cabdd1635b
isensen <isensen@yahoo.cn>
2013-06-28 16:33:19 +0800

    11,12,13

---
+++ src/Two/_16_LinePrinter.java
@@ -0,0 +1,17 @@
+package Two;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: i33
+ * Date: 13-6-28
+ * Time: 下午4:08
+ */
+public class _16_LinePrinter {
+
+    public static void main(String[] args){
+        // Note: \u000A is Unicode representation of linefeed (LF)
+        char c = 0x000A;
+        System.out.println(c);
+    }
+
+}
