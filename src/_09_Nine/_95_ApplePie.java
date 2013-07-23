ead509895de9ba05d84fb35c1a094c51fbfd9a86
isensen <isensen@yahoo.cn>
2013-07-23 09:43:41 +0800

    修改95

--- src/_09_Nine/_95_ApplePie.java
+++ src/_09_Nine/_95_ApplePie.java
@@ -32,42 +32,13 @@ public class _95_ApplePie {
 
 
 
-
+        System.out.println(true?false:true == true?false:true);
 
     }
 
-    //下面这个程序会打印出什么呢？如果你相信的话，前2个程序被报告为系统的缺陷[Bug 4157460 4763901]：
+    //上面这个程序会打印出什么呢？如果你相信的话，前2个程序被报告为系统的缺陷[Bug 4157460 4763901]：
 
     /*
-            public class ApplePie {
-                public static void main(String[] args) {
-                    int count = 0;
-                    for(int i = 0; i < 100; i++); {
-                        count++;
-                    }
-                    System.out.println(count);
-                }
-            }
-
-            import java.util.*;
-            public class BananaBread {
-                public static void main(String[] args) {
-                    Integer[] array = { 3, 1, 4, 1, 5, 9 };
-                    Arrays.sort(array, new Comparator<Integer>() {
-                        public int compare(Integer i1, Integer i2) {
-                            return i1 < i2 ? -1 : (i2 > i1 ? 1 : 0);
-                        }
-                    });
-                   System.out.println(Arrays.toString(array));
-              }
-            }
-
-            public class ChocolateCake {
-                public static void main(String[] args) {
-                     System.out.println(true?false:true == true?false:true);
-                }
-            }
-
     如果你受够这些东西了，那么你不需要知道这些愚蠢谜题的详细解释，所以让我们把它们变得又短又甜：
 
         •	这个程序会打印出1。这是由多余的标号造成的。（分号的恶习？）
